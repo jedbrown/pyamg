ce30859f7732be79961acf4060e7044b242cb0d5
Luke Olson <luke.olson@gmail.com>
2014-11-29 20:34:09 -0600

    Update to -py3 with swig.  Still errors.

--- pyamg/aggregation/aggregate.py
+++ pyamg/aggregation/aggregate.py
@@ -1,8 +1,8 @@
 from __future__ import division
 from __future__ import print_function
+from __future__ import absolute_import
 
 """Aggregation methods"""
-from __future__ import absolute_import
 
 __docformat__ = "restructuredtext en"
 
--- pyamg/relaxation/chebyshev.py
+++ pyamg/relaxation/chebyshev.py
@@ -1,9 +1,9 @@
 from __future__ import division
 from __future__ import print_function
+from __future__ import absolute_import
 
 """Compute coefficients for polynomial smoothers
 """
-from __future__ import absolute_import
 
 import numpy
 
