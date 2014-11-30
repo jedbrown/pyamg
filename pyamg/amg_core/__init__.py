541dded7ac63384a38aaaed45f6777c68a09ebc5
Luke Olson <luke.olson@gmail.com>
2014-11-29 20:01:23 -0600

    futurize amg_core.  leave swig for now

--- pyamg/amg_core/__init__.py
+++ pyamg/amg_core/__init__.py
@@ -1,3 +1,6 @@
 """amg_core - a C++ implementation of AMG-related routines
 """
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 from .amg_core import *
